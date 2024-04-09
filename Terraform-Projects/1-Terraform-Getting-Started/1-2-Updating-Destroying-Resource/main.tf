493b41f40789acf7e99558298df143cc09996601
nobleprogterraform <nobleprogterraform@gmail.com>
2024-04-09 23:05:40 +0000

    commit

--- Terraform-Projects/1-Terraform-Getting-Started/1-2-Updating-Destroying-Resource/main.tf
+++ Terraform-Projects/1-Terraform-Getting-Started/1-2-Updating-Destroying-Resource/main.tf
@@ -2,5 +2,8 @@ resource "local_file" "hcl-basic" {
     filename = "terraform-hcl"
     content = "Terraform introduction"
 
-    file_permission = "0700"
+    #Step 1 first run without uncomment 
+    #Step 2 then uncomment below line and run apply to see resource updating
+    #file_permission = "0700"
+    #Step 3 now run terraform destroy to see destroying resource
 }
